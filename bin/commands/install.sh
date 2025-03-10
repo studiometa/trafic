9aa2eee6eeb98c6da0869cf66b45d8fb30e83c69
Titouan Mathis <titouan@studiometa.fr>
2025-03-10 17:38:41 +0100

    Refactor install script

--- bin/commands/install.sh
+++ bin/commands/install.sh
@@ -1,14 +1,9 @@
-set -e
-
-cd "$(dirname "$0")"
-
-export TRAFIC_ROOT_DIR=$(pwd)
 export DDEV_PROJECT_TLD=${DDEV_PROJECT_TLD:-ikko.dev}
 
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y curl git zip zsh wget vim
 
-for installer in ./install/*.sh; do echo "
+for installer in $TRAFIC_ROOT_DIR/install/*.sh; do echo "
 running $installer...
 " && source $installer; done
