cfa52ed87709bb508c4132e31ab086604a75f940
Titouan Mathis <titouan@studiometa.fr>
2025-03-10 15:40:06 +0100

    Fix migration

---
+++ migrations/1741617641.sh
@@ -0,0 +1,13 @@
+# Remove old Traefik configurations
+sudo rm -f /home/ddev/.config/ddev/traefik/static_config.ddev-server.yaml
+
+# Add the custom Traefik configurations
+sudo mkdir -p /home/ddev/.config/ddev/traefik/config/
+sudo cp $TRAFIC_ROOT_DIR/config/traefik/static_config.trafic.yaml /home/ddev/.config/ddev/traefik/
+sudo chown ddev:ddev /home/ddev/.config/ddev/traefik/static_config.trafic.yaml
+sudo cp $TRAFIC_ROOT_DIR/config/traefik/default_config.yaml /home/ddev/.config/ddev/traefik/config/
+sudo chown ddev:ddev /home/ddev/.config/ddev/traefik/config/default_config.yaml
+
+DDEV_PROJECT_TO_START=$(sudo su - ddev -c "ddev ls --json-output --active-only | jq -r '.raw[0].name'")
+sudo su - ddev -c "ddev poweroff"
+sudo su - ddev -c "ddev start $DDEV_PROJECT_TO_START"
