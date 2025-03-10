c630ba2d4aef93beb506174d2fdc9e4eac2b6197
Titouan Mathis <titouan@studiometa.fr>
2025-03-10 12:01:03 +0100

    Add a command

---
+++ middleware/src/Command/UpdateLastAccessCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\DDEV;
+use App\Service\Redis;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'ddev:update-last-access',
+    description: 'Update last access time for a given DDEV project'
+)]
+class UpdateLastAccessCommand extends Command
+{
+    public function __construct(
+        private Redis $redis,
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('project', InputArgument::REQUIRED, 'The project to update.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $host = $input->getFirstArgument();
+
+        if (!$host || !$this->redis->exists($host)) {
+            return Command::INVALID;
+        }
+
+        $project = $this->redis->get($host);
+        $project['last_accessed_at'] = time();
+        $this->redis->set($host, $project);
+
+        return Command::SUCCESS;
+    }
+}
