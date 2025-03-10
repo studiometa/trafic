a5ff548ebe702f33575dbeb673bbb998629b31dd
Titouan Mathis <titouan@studiometa.fr>
2025-03-10 15:07:40 +0100

    Remove unnecessary logs

--- bin/commands/update.sh
+++ bin/commands/update.sh
@@ -7,9 +7,7 @@ git pull
 for FILE in $TRAFFIC_ROOT_DIR/migrations/*.sh; do
 	FILENAME=$(basename "$FILE")
 	MIGRATE_AT="${FILENAME%.sh}"
-	echo "$FILE"
-	echo "$FILENAME"
-	echo "$MIGRATE_AT"
+
 	if [[ $MIGRATE_AT -gt $LAST_UPDATED_AT ]]
 	then
 		echo "Running migration for $MIGRATE_AT..."
