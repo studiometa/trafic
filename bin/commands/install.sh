8ee89e93bca5dc84338162e325735272c45f92eb
Titouan Mathis <titouan@studiometa.fr>
2025-03-11 14:39:33 +0100

    Add ncdu to default packages

--- bin/commands/install.sh
+++ bin/commands/install.sh
@@ -1,9 +1,5 @@
 export DDEV_PROJECT_TLD=${DDEV_PROJECT_TLD:-ikko.dev}
 
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y curl git zip zsh wget vim
-
 for installer in $TRAFIC_ROOT_DIR/install/*.sh; do echo "
 running $installer...
 " && source $installer; done
