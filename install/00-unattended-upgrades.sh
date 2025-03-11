0bfb6d6f066aed25637fa4d0442abf22860625b7
Titouan Mathis <titouan@studiometa.fr>
2025-03-11 15:17:18 +0100

    Fix install

--- install/00-unattended-upgrades.sh
+++ install/00-unattended-upgrades.sh
@@ -1,8 +1,8 @@
-sudo apt-get install -y --force-yes unattended-upgrades
+sudo apt-get install -y unattended-upgrades
 
-echo '''
-APT::Periodic::Update-Package-Lists "1";
+echo '''APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Download-Upgradeable-Packages "1";
 APT::Periodic::AutocleanInterval "7";
-APT::Periodic::Unattended-Upgrade "1";
-''' | sudo tee /etc/apt/apt.conf.d/10periodic
+APT::Periodic::Unattended-Upgrade "1";''' | sudo tee /etc/apt/apt.conf.d/10periodic
+
+sudo systemctl start unattended-upgrades
