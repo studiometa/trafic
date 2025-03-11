617bb9dc6aaf3ade26324c499ac6f205331a36b3
Titouan Mathis <titouan@studiometa.fr>
2025-03-11 10:09:39 +0100

    Cleanup

--- install/01-ddev.sh
+++ install/01-ddev.sh
@@ -1,16 +1,13 @@
 # Add DDEV’s GPG key to your keyring
-sudo sh -c 'echo ""'
 sudo apt update && sudo apt install -y curl gpg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://pkg.ddev.com/apt/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/ddev.gpg > /dev/null
 sudo chmod a+r /etc/apt/keyrings/ddev.gpg
 
 # Add DDEV releases to your package repository
-sudo sh -c 'echo ""'
 echo "deb [signed-by=/etc/apt/keyrings/ddev.gpg] https://pkg.ddev.com/apt/ * *" | sudo tee /etc/apt/sources.list.d/ddev.list >/dev/null
 
 # Update package information and install DDEV
-sudo sh -c 'echo ""'
 sudo apt update
 sudo apt install -y ddev
 
